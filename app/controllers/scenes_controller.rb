e3e68dd932ed5591dbe0a9172fb314cc5f8c4526
leichaofan <leichaofan2024@gmail.com>
2017-06-06 22:37:00 +0800

    generate scene model and implement CRUD

---
+++ app/controllers/scenes_controller.rb
@@ -0,0 +1,50 @@
+class ScenesController < ApplicationController
+  before_action :find_scene, only: [:show, :edit , :update, :destroy]
+
+  def index
+    @scenes = Scene.all
+  end
+
+  def show
+  end
+
+  def new
+    @scene = Scene.new
+  end
+
+  def create
+    @scene = Scene.new(scene_params)
+    if @scene.save
+      redirect_to scenes_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @scene.update(scene_params)
+      redirect_to scenes_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @scene.destroy
+    redirect_to scenes_path, alert: "你已成功删除此场景"
+  end
+
+  private
+
+  def scene_params
+    params.require(:scene).permit(:title)
+  end
+
+  def find_scene
+    @scene = Scene.find(params[:id])
+  end 
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170605004400) do
+ActiveRecord::Schema.define(version: 20170606134035) do
+
+  create_table "keys", force: :cascade do |t|
+    t.boolean  "key1",            default: false
+    t.boolean  "key2",            default: false
+    t.boolean  "key3",            default: false
+    t.string   "key1name"
+    t.string   "key2name"
+    t.string   "key3name"
+    t.text     "key1description"
+    t.text     "key2description"
+    t.text     "key3description"
+    t.string   "scenes"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+  end
+
+  create_table "lights", force: :cascade do |t|
+    t.string   "light_status"
+    t.integer  "key_id"
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "scenes", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
